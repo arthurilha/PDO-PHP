9513073bff4661413d4f541e92ee798c6963d300
unknown <arthur.ilha20@gmail.com>
2023-01-06 19:04:10 -0300

    deleteANDupdate

--- crud.php
+++ crud.php
@@ -1,6 +1,6 @@
 
 <?php 
-
+//---------------------CONEXAO-----------------------------////////
 try{
     $pdo = new PDO("mysql:dbname=ESTUDO;host=localhost", "root","");
 }catch(PDOException $e){
@@ -30,8 +30,30 @@ VALUES (:nome, :telefone, :email)");//precisa de parametro
 
 
 //3°forma de inserir
-$pdo->query("INSERT INTO pessoa(nome, telefone, email)
-VALUES ('João','51923232323', 'joao@gmail.com')");
+//$pdo->query("INSERT INTO pessoa(nome, telefone, email)
+//VALUES ('João','51923232323', 'joao@gmail.com')");
 
 
+
+//------------------------------DELETE---------------////
+
+//1°forma de deletar
+//$res = $pdo -> prepare("DELETE FROM pessoa Where id = :id");
+//$id = 2;
+//$res -> bindValue(":id",$id);
+//$res -> execute();
+
+//2°forma de deletar
+//$pdo -> query("DELETE FROM pessoa WHERE id = '3'");
+
+
+//------------------------------UPDATE---------------////
+
+//$res = $pdo -> prepare("UPDATE pessoa SET email = :email where id = :id");
+//$res -> bindValue(":email","Joao02@gmail.com");
+//$res -> bindValue(":id",'4');
+//$res -> execute();
+
+
+//$pdo -> query("UPDATE pessoa set email = 'joaoBnana@gmail.com' WHERE id = '5'")
 ?>
